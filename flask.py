02d8f486f07d8a8bcabacce6726495959e6c0059
hydefbla <anjanbharadwaj02@gmail.com>
2017-11-19 22:06:02 -0800

    push
    1

---
+++ flask.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_mail import Mail
+from flask_mail import Message
+
+app = Flask(__name__)
+mail = Mail(app)
+@app.route('/flask/<name>/<email>/<message>', methods=['POST'])
+def handle_data(name,email,message):
+    msg = Message(message,
+                  subject=name
+                  sender=email,
+                  recipients=["anjanbharadwaj02@gmail.com"])
+    mail.send(msg)
+    # your code
+    # return a response
+@app.route("/upload/<documentName>", methods=['POST'])
+def upload(documentName):
+    with open(os.path.join("/home/pi/Documents/WebServer/dropbox/", documentName), 'wb') as temp_file:
+        temp_file.write(buff)
+
+    return documentName
